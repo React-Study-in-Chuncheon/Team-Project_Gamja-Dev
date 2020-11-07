053058d4736d0559382960d1035d5d62739959ea
Dolphin-PC <pcx474@gmail.com>
2020-11-07 20:13:33 +0900

    Update LandingView.jsx
    Add a dispatch

--- src/views/LandingView.jsx
+++ src/views/LandingView.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { TEST_TYPE } from "../reducer/type";
 
 const LandingView = () => {
    // state[index.js 안에 있는 combineReducer 이름][각 Reducer의 키 값]
    const reduxData = useSelector((state) => state.test.test);
+   const dispatch = useDispatch();
 
    console.info(reduxData);
 
-   return <div>{reduxData}</div>;
+   const onClickButton = () => {
+      dispatch({
+         type: TEST_TYPE,
+         payload: "OK Bye~!",
+      });
+   };
+
+   return (
+      <div>
+         {reduxData}
+         <button onClick={onClickButton}>click me</button>
+      </div>
+   );
 };
 
 export default LandingView;
